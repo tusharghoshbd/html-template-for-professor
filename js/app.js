ca2234e58161f210758bdd153cba73c0a0c35d26
Tushar Ghosh <tushar.ghosh@bjitgroup.com>
2019-04-03 16:47:43 +0600

    create html template for university professor

---
+++ js/app.js
@@ -0,0 +1,52 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', ['ngRoute','ngAnimate']);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			});
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', ["$scope", function($scope) {
+		// create a message to display in our view
+		console.log("home");
+		$scope.message = 'Everyone come and see how good I look!';
+	}]);
+
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
+
+	scotchApp.directive('flexslider', function () {
+  
+	  return {
+	    link: function (scope, element, attrs) {
+	      
+	      element.flexslider({
+	        animation: "slide"
+	      });
+	    }
+	  }
+	});
